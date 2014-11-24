9b3c83b2b120cbba01f0fd068705529fc9db2615
Ting-Yuan Hsia <ly2314@gmail.com>
2014-11-24 19:51:01 +0800

    Add AWS config sample

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+    return array(
+        'includes' => array('_aws'),
+        'services' => array(
+            'default_settings' => array(
+                'params' => array(
+                    'key'    => 'ENTER_YOUR_IAM_KEY_HERE',
+                    'secret' => 'ENTER_YOUR_IAM_SECRET_HERE',
+                    'region' => 'ap-southeast-1'
+                )
+            )
+        )
+);
